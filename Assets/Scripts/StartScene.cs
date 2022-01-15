e05e59125e9a0ee18a0e0aafe317afbea26db9cb
junho-a1 <jzzazza@naver.com>
2022-01-15 12:53:40 +0900

    ee
    wqw

---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+
+    public void StartBt()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void ExplanationBt()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void CreditBt()
+    {
+        SceneManager.LoadScene("");
+    }
+}
