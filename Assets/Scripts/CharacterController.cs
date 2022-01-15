4e7136b7452c079365abb3737618d8dd8c5938a3
77Sun77 <kagfhdjsk57@gmail.com>
2022-01-15 13:22:32 +0900

    캐릭터 움직임

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public GameObject player1, player2;
+    public Rigidbody2D rigid1, rigid2;
+    public float maxSpeed;
+    Vector2 mousePos;
     void Start()
     {
         
@@ -12,19 +15,28 @@ public class CharacterController : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetMouseButtonDown(1))
+        {
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+    }
+
+    private void FixedUpdate()
+    {
         player1Move();
         player2Move();
     }
 
     void player1Move()
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        player1.transform.Translate(new Vector2(x, y) * 4 * Time.deltaTime);
+        float h = Input.GetAxis("Horizontal");
+        float w = Input.GetAxis("Vertical");
+        rigid1.velocity = new Vector2(h, w) * maxSpeed;
     }
 
     void player2Move()
     {
-
+        player2.transform.position = Vector3.Lerp(player2.transform.position, mousePos, (maxSpeed-2) * Time.deltaTime);
     }
 }
