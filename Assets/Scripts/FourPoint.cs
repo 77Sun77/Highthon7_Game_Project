39c68043b1adbcbb733445d01eadcdd33ff994d2
junho-a1 <jzzazza@naver.com>
2022-01-16 02:11:11 +0900

    w
    www

--- Assets/Scripts/FourPoint.cs
+++ Assets/Scripts/FourPoint.cs
@@ -6,11 +6,18 @@ public class FourPoint : MonoBehaviour
 {
     public static int truePoint = 0;
     public static bool isPoint = false;
+    public GameObject go;
 
     public SpriteRenderer bt1;
     public SpriteRenderer bt2;
     public SpriteRenderer bt3;
     public SpriteRenderer bt4;
+
+    public GameObject Shutter1;
+    public GameObject Shutter2;
+    public GameObject Shutter3;
+    public GameObject Shutter4;
+
     void Start()
     {
     }
@@ -23,22 +30,27 @@ public class FourPoint : MonoBehaviour
             {
                 bt1.color = new Color(142 / 255f, 142 / 255f, 142 / 255f, 255 / 255f);
                 truePoint++;
+                Shutter1.SetActive(false);
             }
             else if (collision.tag == "left" && truePoint == 1)
             {
                 bt2.color = new Color(142 / 255f, 142 / 255f, 142 / 255f, 255 / 255f);
                 truePoint++;
+                Shutter2.SetActive(false);
             }
             else if (collision.tag == "right" && truePoint == 2)
             {
                 bt3.color = new Color(142 / 255f, 142 / 255f, 142 / 255f, 255 / 255f);
                 truePoint++;
+                Shutter3.SetActive(false);
             }
             else if (collision.tag == "up" && truePoint == 3)
             {
                 bt4.color = new Color(142 / 255f, 142 / 255f, 142 / 255f, 255 / 255f);
                 truePoint++;
-                Debug.Log("클리어");
+                Shutter4.SetActive(false);
+                go.SetActive(false);
+                isPoint = false;
             }
             else
             {
@@ -46,6 +58,10 @@ public class FourPoint : MonoBehaviour
                 bt2.color = new Color(255 / 255f, 255 / 255f, 255 / 255f, 255 / 255f);
                 bt3.color = new Color(255 / 255f, 255 / 255f, 255 / 255f, 255 / 255f);
                 bt4.color = new Color(255 / 255f, 255 / 255f, 255 / 255f, 255 / 255f);
+                Shutter4.SetActive(true);
+                Shutter4.SetActive(true);
+                Shutter4.SetActive(true);
+                Shutter4.SetActive(true);
                 truePoint = 0;
                 ProblemKinds.FailStac++;
                 Debug.Log("실패");
